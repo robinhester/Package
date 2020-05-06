fe49f98c7220bd677f2e5cc95b34e7ebd0f0f1d2
robinhester <robin.hester@yahoo.com>
2020-05-06 17:48:52 -0400

    package

--- lambdata_robinhester/setup.py
+++ lambdata_robinhester/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata_robinhester", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Robin Hester",
     author_email="robin.hester@yahoo.com",
     description="Functions that can used with predictive modeling",
